004d04371d90e5f29fcf8f598b77347fecf12755
Mikel Cobián <mikelcobian@egmail.com>
2021-05-27 20:44:59 +0200

    commit inicial

---
+++ inicio/src/test/java/com/libreria/inicio/InicioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.libreria.inicio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InicioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
